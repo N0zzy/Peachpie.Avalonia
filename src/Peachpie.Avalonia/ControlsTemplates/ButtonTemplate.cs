7cf69e94fbd12196234a4ad11bbf57eba26bd6fd
FibonacciFox <shamanwolfrus@gmail.com>
2022-11-23 17:53:18 +0300

    add base controls

---
+++ src/Peachpie.Avalonia/ControlsTemplates/ButtonTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Styling;
+using Pchp.Core;
+
+namespace Peachpie.Avalonia.ControlsTemplates;
+
+/// <summary>
+/// A standard button control.
+/// </summary>
+public class ButtonTemplate : Button , IStyleable
+{
+    [PhpHidden] private Type StyleKey = typeof(Button);
+
+    public ButtonTemplate()
+    {
+        Content = "ButtonTemplate";
+    }
+}
---
+++ src/Peachpie.Avalonia/ControlsTemplates/WindowTemplate.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Peachpie.Avalonia.ControlsTemplates;
+
+/// <summary>
+/// A top-level window.
+/// </summary>
+public class WindowTemplate : Window
+{
+    
+}
