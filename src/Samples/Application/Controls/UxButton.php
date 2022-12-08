ef590336b4f9cf92d4f2c5a1c0d95b49b27fdee2
FibonacciFox <shamanwolfrus@gmail.com>
2022-12-08 11:17:58 +0700

    Add Samples

---
+++ src/Samples/Application/Controls/UxButton.php
@@ -0,0 +1,9 @@
+﻿<?php
+
+namespace Controls;
+
+use Peachpie\Avalonia\ControlsTemplates\ButtonTemplate;
+
+class UxButton extends ButtonTemplate {
+    
+}
---
+++ src/Samples/ApplicationDesktop/App.php
@@ -0,0 +1,6 @@
+﻿<?php
+
+use Controls\UxButton;
+
+$button  = new UxButton();
+echo $button;
