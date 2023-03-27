eeed9f2d4d0c57fa2dc978c4924b6a69341b7da6
FibonacciFox <shamanwolfrus@gmail.com>
2023-03-27 13:29:53 +0300

    Added UxViewbox.

---
+++ src/Peachpie.Avalonia/Controls/UxViewbox.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Controls;
+
+namespace Peachpie.Avalonia.Controls;
+
+/// <summary>
+/// Viewbox is used to scale single child to fit in the available space.
+/// </summary>
+public class UxViewbox<T> : UxControl<T> where T : Viewbox
+{
+    
+}
+
+/// <summary>
+/// Viewbox is used to scale single child to fit in the available space.
+/// </summary>
+public class UxViewbox : UxViewbox<Viewbox>
+{
+    
+}
---
+++ src/Samples/SharpPie/Peachpie/Avalonia/Controls/UxViewbox.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Peachpie\Avalonia\Controls;
+
+/**
+ * UxViewbox is used to scale single child to fit in the available space.
+ *
+ */
+class UxViewbox extends UxControl
+{
+
+}
